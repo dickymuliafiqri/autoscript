f7f6e9d0839b0e1cfd7ead13b14bcc959231b7f2
Dugong <110967975+dugong-lewat@users.noreply.github.com>
2023-12-16 03:11:08 +0700

    Add files via upload

---
+++ other/about.sh
@@ -0,0 +1,32 @@
+NC='\e[0m'
+DEFBOLD='\e[39;1m'
+RB='\e[31;1m'
+GB='\e[32;1m'
+YB='\e[33;1m'
+BB='\e[34;1m'
+MB='\e[35;1m'
+CB='\e[35;1m'
+WB='\e[37;1m'
+clear
+echo ""
+echo ""
+echo -e "${BB}—————————————————————————————————————————————————————————${NC}"
+echo -e "                  ${WB}MINI SCRIPT BY DUGONG${NC}"
+echo -e "${BB}—————————————————————————————————————————————————————————${NC}"
+echo -e "  ${WB}»»» Protocol Service «««  |  »»» Network Protocol «««${NC}  "
+echo -e "${BB}—————————————————————————————————————————————————————————${NC}"
+echo -e "  ${YB}- Vless${NC}                   ${WB}|${NC}  ${YB}- Websocket (CDN) non TLS${NC}"
+echo -e "  ${YB}- Vmess${NC}                   ${WB}|${NC}  ${YB}- Websocket (CDN) TLS${NC}"
+echo -e "  ${YB}- Trojan${NC}                  ${WB}|${NC}  ${YB}- gRPC (CDN) TLS${NC}"
+echo -e "  ${YB}- Socks5${NC}                  ${WB}|${NC}"
+echo -e "  ${YB}- Shadowsocks${NC}             ${WB}|${NC}"
+echo -e "  ${YB}- Shadowsocks 2022${NC}        ${WB}|${NC}"
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e "               ${WB}»»» Network Port Service «««${NC}             "
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e "  ${YB}- HTTPS : 443, 2053, 2083, 2087, 2096, 8443${NC}"
+echo -e "  ${YB}- HTTP  : 80, 8080, 8880, 2052, 2082, 2086, 2095${NC}"
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo ""
+read -n 1 -s -r -p "Press any key to back on menu"
+menu
